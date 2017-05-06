53621e2ad9c7f5a2dd21393acf387e510561dfc4
ripper <ricardo.pcan@gmail.com>
2017-05-06 00:51:35 -0500

    Init repo

---
+++ script.sh
@@ -0,0 +1,19 @@
+wget -O ~/.vocab "https://goo.gl/N4EiTq" --no-check-certificate
+wget -O ~/.vocabscript "https://goo.gl/gI7xKQ" --no-check-certificate
+
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+	OSBASHRC=bashrc
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+	OSBASHRC=bash_profile
+fi
+
+if ! grep -Fxq '~/.vocab' ~/.$OSBASHRC; then
+	echo $'\nchmod +x ~/.vocab\n~/.vocab' >> ~/.$OSBASHRC
+fi
+
+OSBASHRC="zshrc"
+if [[ -f ~/.$OSBASHRC ]]; then
+	if ! grep -Fxq '~/.vocab' ~/.$OSBASHRC; then
+		echo $'\nchmod +x ~/.vocab\n~/.vocab' >> ~/.$OSBASHRC
+	fi
+fi
